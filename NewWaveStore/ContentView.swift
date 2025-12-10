285947be0988275f8fc3f0a647ac6f4d22d6de45
willw008 <wilylosu@gmail.com>
2025-12-10 11:06:38 -0500

    Initial Commit

---
+++ NewWaveStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewWaveStore
+//
+//  Created by William Bomar (student LM) on 12/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewWaveStore/NewWaveStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewWaveStoreApp.swift
+//  NewWaveStore
+//
+//  Created by William Bomar (student LM) on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NewWaveStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
